510b45c592583738b9bc25b7cb54cd1d0da45164
suriya8187 <47661281+suriya8187@users.noreply.github.com>
2019-02-18 15:13:09 +0530

    Update HelloWorld.java

--- SampleArtifact/src/test/java/HelloWorld.java
+++ SampleArtifact/src/test/java/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi Hello World");
+		System.out.println("Hi Jenkins trigger webhook");
+
 	}
 
 }
